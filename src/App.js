ae1b0dd585efe6ba45fcb19acfe9708d1205b752
Weslen Mendes <weslenmends@gmail.com>
2021-09-15 11:15:45 -0300

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched">Wesl</span>
+        <span className="remainder">en</span>
+      </div>
+      <div className="typed-keys">asdfWeslasdf</div>
+      <div className="completed-words">
+        <ol>
+          <li>cidade</li>
+          <li>carro</li>
+          <li>profissional</li>
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
